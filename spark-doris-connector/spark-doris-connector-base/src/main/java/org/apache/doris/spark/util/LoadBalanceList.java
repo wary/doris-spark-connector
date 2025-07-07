1d7a74771d94fe4a2d40a4bdb0b41904f08377ba
LeiWang <leihz1989@gmail.com>
2025-07-07 15:24:12 +0800

    upgrade to 25.1.0.7-MUSIC-RELEASE

--- spark-doris-connector/spark-doris-connector-base/src/main/java/org/apache/doris/spark/util/LoadBalanceList.java
+++ spark-doris-connector/spark-doris-connector-base/src/main/java/org/apache/doris/spark/util/LoadBalanceList.java
@@ -17,6 +17,7 @@
 
 package org.apache.doris.spark.util;
 
+import java.io.Serializable;
 import java.util.Map;
 import java.util.List;
 import java.util.Iterator;
@@ -28,7 +29,7 @@ import java.util.Collections;
 import java.util.concurrent.ConcurrentHashMap;
 
 
-public class LoadBalanceList<T> implements Iterable<T> {
+public class LoadBalanceList<T> implements Iterable<T>, Serializable {
 
 	private final List<T> list;
 
@@ -82,7 +83,7 @@ public class LoadBalanceList<T> implements Iterable<T> {
 		this.failedServers.put(server, new FailedServer<T>(server));
 	}
 
-	private static class FailedServer<T> implements Comparable<FailedServer<T>> {
+	private static class FailedServer<T> implements Comparable<FailedServer<T>>, Serializable {
 
 		protected final T server;
 
